3caca7945392bb8a8879e40003a52ecf908f752e
mlbb220 <mlbb220@8e60345e-f60f-32fb-936b-aef682da3172>
2010-04-30 03:03:54 +0000

    test commit file

--- src/rich/calendar/TestWeekDays.java
+++ src/rich/calendar/TestWeekDays.java
@@ -9,6 +9,7 @@ public class TestWeekDays {
 	@Test
 	public void dayOfWeek(){
 		Calendar c = Calendar.getInstance();
-		System.out.println(c.getTime().getDay());
+		System.out.println(c.get(Calendar.DAY_OF_WEEK));
+		System.out.println(java.util.Calendar.getInstance().get(java.util.Calendar.DATE));
 	}
 }
